c65362a93ffcfe342567c1bd78e0c82ad8205f31
Minusi <minusie357@gmail.com>
2023-04-08 16:17:02 +0900

    Time: 21 ms (75.45%), Space: 11.7 MB (17.88%) - LeetHub

---
+++ 000n-ary-tree-preorder-traversal/000n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        std::vector<int> result;
+        
+        std::list<Node*> nodeStack;
+
+        nodeStack.push_back(root);
+        while(nodeStack.empty() == false)
+        {
+            auto node = nodeStack.back();
+            nodeStack.pop_back();
+            if(node == nullptr)
+            {
+                continue;
+            }
+
+            result.push_back(node->val);
+            for(auto rit = node->children.rbegin(); rit != node->children.rend(); ++rit)
+            {
+                nodeStack.push_back(*rit);
+            }
+        }
+
+        return result;
+    }
+};
