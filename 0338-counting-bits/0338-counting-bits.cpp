13d734cb4e5d2d5af7d4c57efc3517d000db7a18
Minusi <minusie357@gmail.com>
2023-09-15 19:09:12 +0900

    Time: 3 ms (84.1%), Space: 8.8 MB (6.76%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        std::vector<int> result;
+        result.push_back(0);
+        if (n == 0) 
+        {
+            return result;
+        }
+        result.push_back(1);
+        if (n == 1)
+        {
+            return result;
+        }
+
+        auto num = 2;
+        while (num <= n)
+        {
+            auto size = result.size();
+            for (auto index = 0; index < size && size + index <= n; ++index)
+            {
+                result.push_back(1 + result[index]);
+            }
+
+            num *= 2;
+        }
+
+        return result;
+    }
+};
