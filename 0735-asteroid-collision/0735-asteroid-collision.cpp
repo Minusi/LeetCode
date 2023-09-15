db3dd8e89265698e4adbe8a015581ed20bc4b494
Minusi <minusie357@gmail.com>
2023-09-15 19:37:05 +0900

    Time: 8 ms (92.6%), Space: 21.6 MB (7.5%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        std::vector<int> result;
+        result.reserve(10000);
+
+        for (auto asteroid : asteroids)
+        {
+            if (result.empty())
+            {
+                result.push_back(asteroid);
+                continue;
+            }
+
+            bool newAsteroidAlive = true;
+            while (result.empty() == false)
+            {
+                auto lastAsteroid = result.back();
+                if ((lastAsteroid >= 0 && asteroid >= 0)
+                    || (lastAsteroid < 0 && asteroid < 0)
+                    || (lastAsteroid < 0 && asteroid > 0))
+                {
+                    break;
+                }
+
+                if (abs(lastAsteroid) > abs(asteroid))
+                {
+                    newAsteroidAlive = false;
+                    break;
+                }
+                else if (abs(lastAsteroid) == abs(asteroid))
+                {
+                    newAsteroidAlive = false;
+                    result.pop_back();
+                    break;
+                }
+                else
+                {
+                    result.pop_back();
+                }
+            }
+
+            if (newAsteroidAlive)
+            {
+                result.push_back(asteroid);
+            }
+        }
+
+        return result;
+    }
+};
